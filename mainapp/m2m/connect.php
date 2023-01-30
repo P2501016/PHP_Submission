bdc284bffe0f8f6a32d9a4eab05cdeb59c2771b8
nashibl <88990054+nashibl@users.noreply.github.com>
2023-01-30 11:13:27 +0000

    Delete mainapp/m2m directory

--- mainapp/m2m/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $wsdl = 'https://m2mconnect.ee.co.uk/orange-soap/services/MessageServiceByCountry?wsdl';
-    $client = new SoapClient($wsdl);
-    echo "Successfully connected to M2MConnect.";
-} catch (Exception $e) {
-    echo 'Error: ' . $e->getMessage();
-}
-?>
--- mainapp/m2m/pdocheck.php
+++
@@ -1,7 +0,0 @@
-<?php
-print_r(PDO::getAvailableDrivers());
-if(in_array("mysql",PDO::getAvailableDrivers())){
-    echo " You have PDO for MySQL driver installed ";
-     }else{
-    echo "PDO driver for MySQL is not installed in your system";
-     }
